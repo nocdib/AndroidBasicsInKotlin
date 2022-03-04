c4ca1fd9b584df4666706ad6391d00e5f89b9615
Gregory Joshua <nocdib@gmail.com>
2022-03-04 15:56:49 +0100

    Start with Write Unit Tests

--- 01_Dice_Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ 01_Dice_Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.diceroller
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,18 +11,35 @@ class MainActivity : AppCompatActivity() {
 
     val numSides = 6
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val rollButton: Button = findViewById(R.id.button)
-        val resultTextView: TextView = findViewById(R.id.textView)
+        rollButtonClick()
 
         // When the Roll button is clicked, display the dice value in the TextView
         rollButton.setOnClickListener {
-            resultTextView.text = rollDice(numSides)
+            rollButtonClick()
         }
     }
 
-    private fun rollDice(sides: Int): String = Dice(sides).roll().toString()
+    fun rollButtonClick() {
+        var content = rollDice()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(content)
+        diceImage.contentDescription = content.toString()
+    }
+
+    private fun rollDice(): Int {
+        val drawable = when(Dice(numSides).roll()) {
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.dice_1
+        }
+        return drawable
+    }
 }
